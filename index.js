a2d4c14ab9ee62cce514b7ae2500d1f916973ca5
CamillaOliveiraSouza <74692695+CamillaOliveiraSouza@users.noreply.github.com>
2020-11-18 23:17:53 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+function operacoes_basicas(x, y){
+
+  let soma = x + y
+  let subtracao = x - y
+  let multiplicacao = x * y 
+  let divisao = x / y 
+
+  console.log(`O valor da soma de ${x} e ${y} é: ${soma}`)
+  console.log(`O valor da subtracao de ${x} e ${y} é: ${subtracao}`)
+  console.log(`O valor da multiplicacao de ${x} e ${y} é: ${multiplicacao}`)
+  console.log(`O valor da divisao de ${x} e ${y} é: ${divisao}`)
+  console.log("")
+}
+
+operacoes_basicas(1,0)
+operacoes_basicas(11, 10)
